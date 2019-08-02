ab23dba9386337759a1f9fca1d1fc3887249cf7f
MilanTodorovic <todor-90@hotmail.com>
2019-08-02 23:25:47 +0200

    Database and Images

--- YGO/Assets/Scripts/loadFromDatabase.cs
+++ YGO/Assets/Scripts/loadFromDatabase.cs
@@ -9,7 +9,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        string connection = "URI=file:" + Application.persistentDataPath + "/" + "My_Database";
+        //IDbConnection dbcon = new SqliteConnection(connection);
+        //dbcon.Open();
+        // do something
+        //dbcon.Close();
     }
 
     // Update is called once per frame
