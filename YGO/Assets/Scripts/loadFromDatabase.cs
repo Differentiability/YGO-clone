a3f59133bf6800a7130253d7052dea9db9143a46
MilanTodorovic <todor-90@hotmail.com>
2019-08-02 23:46:59 +0200

    Database script

--- YGO/Assets/Scripts/loadFromDatabase.cs
+++ YGO/Assets/Scripts/loadFromDatabase.cs
@@ -4,21 +4,25 @@ using System.Data;
 using System.IO;
 
 //https://github.com/rizasif/sqlite-unity-plugin
-public class NewBehaviourScript : MonoBehaviour
+public class loadFromDatabase : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        string connection = "URI=file:" + Application.persistentDataPath + "/" + "My_Database";
-        //IDbConnection dbcon = new SqliteConnection(connection);
-        //dbcon.Open();
-        // do something
-        //dbcon.Close();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        try
+        {
+            string app_path = Application.dataPath;//Application.persistentDataPath;
+            Debug.Log(app_path);
+            string connection = "URI=file:" + app_path + "/" + "Database/card_list.sqlite";
+            IDbConnection dbcon = new SqliteConnection(connection);
+            dbcon.Open();
+            // do something
+            dbcon.Close();
+        }
+        catch (System.Exception e)
+        {
+            //Debug.Log("Naslo bazu!");
+            Debug.Log(e);
+        };
     }
 }
